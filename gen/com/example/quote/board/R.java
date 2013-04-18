55893aa8a8a09e0859b693333fd7a21aa21cf2a7
syedbdusa <syedbdusa@gmail.com>
2013-04-17 22:33:32 -0400

    I aligned the buttons perfectly, but hamza i still need some new images for the buttons, pressed and normal

--- gen/com/example/quote/board/R.java
+++ gen/com/example/quote/board/R.java
@@ -21,8 +21,8 @@ public final class R {
     public static final class id {
         public static final int info=0x7f080005;
         public static final int linearLayout1=0x7f080002;
-        public static final int lowC=0x7f080000;
-        public static final int lowCsharp=0x7f080001;
+        public static final int lowC=0x7f080001;
+        public static final int lowCsharp=0x7f080000;
         public static final int menu_settings=0x7f080006;
         public static final int specialKeyboards=0x7f080004;
         public static final int startPlaying=0x7f080003;
